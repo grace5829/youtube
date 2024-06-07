55b5fa56676aba11cf07a007c01ff5b58d9e2f54
Grace Gao <grace5829@gmail.com>
2024-06-07 18:44:15 -0400

    updating bucket name

--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -5,8 +5,8 @@ import ffmpeg from 'fluent-ffmpeg';
 
 const storage = new Storage();
 
-const rawVideoBucketName = "neetcode-yt-raw-videos";
-const processedVideoBucketName = "neetcode-yt-processed-videos";
+const rawVideoBucketName = "grace-yt-raw-videos";
+const processedVideoBucketName = "grace-yt-processed-videos";
 
 const localRawVideoPath = "./raw-videos";
 const localProcessedVideoPath = "./processed-videos";
